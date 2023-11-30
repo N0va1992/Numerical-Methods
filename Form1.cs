1d1c3461fe9e359ecaa0dec4bc0bd8f005997a6f
Nova92-py <120581606+Nova92-py@users.noreply.github.com>
2023-11-30 10:11:14 +0100

    Revert "added some laguage properties. dont work"
    This reverts commit 8fab19e29514b0dcc30a00803d0aaf1442922502.

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,12 @@
-﻿using MetodyNumeryczne;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace metodyNumeryczne
 {
@@ -18,33 +15,8 @@ namespace metodyNumeryczne
         public Form1()
         {
             InitializeComponent();
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.Items.Add("en-US");
-            comboBox1.Items.Add("pl-PL");
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Ustaw domyślny język
-            LanguageManager.Initialize("en-US");
-            UpdateUI();
-        }
-
-        private void UpdateUI()
-        {
-            title.Text = LanguageManager.GetLocalizedString("Label1Text");
-            approximationsBtn.Text = LanguageManager.GetLocalizedString("Approximations");
-            // Dodaj kolejne kontrolki, które chcesz tłumaczyć
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (comboBox1.SelectedItem != null)
-            {
-                string selectedLanguageCode = comboBox1.SelectedItem.ToString();
-                LanguageManager.Initialize(selectedLanguageCode);
-                UpdateUI();
-            }
+            title.Text = "Wstep do Metod Numerycznych" +
+                "\n Aplikacja komputerowa";
         }
     }
 }

1fdeb7adb96794578096906dbaecdf58edea306c
N0va1992 <bekierpatryk311@gmail.com>
2023-12-05 13:05:10 +0100

    need to repair calculate function

--- InterpolationMenuControl.Designer.cs
+++ InterpolationMenuControl.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.interpolationTitle = new System.Windows.Forms.Label();
             this.backBtn = new System.Windows.Forms.Button();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.fxLabel = new System.Windows.Forms.Label();
+            this.interLabel = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.fxTextBox = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.interTaskText = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // interpolationTitle
@@ -54,15 +62,95 @@
             this.backBtn.UseVisualStyleBackColor = true;
             this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
             // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(3, 140);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(15, 13);
+            this.xLabel.TabIndex = 2;
+            this.xLabel.Text = "x:";
+            // 
+            // fxLabel
+            // 
+            this.fxLabel.AutoSize = true;
+            this.fxLabel.Location = new System.Drawing.Point(2, 178);
+            this.fxLabel.Name = "fxLabel";
+            this.fxLabel.Size = new System.Drawing.Size(24, 13);
+            this.fxLabel.TabIndex = 3;
+            this.fxLabel.Text = "f(x):";
+            // 
+            // interLabel
+            // 
+            this.interLabel.AutoSize = true;
+            this.interLabel.Location = new System.Drawing.Point(3, 261);
+            this.interLabel.Name = "interLabel";
+            this.interLabel.Size = new System.Drawing.Size(126, 13);
+            this.interLabel.TabIndex = 4;
+            this.interLabel.Text = "Wielomian interpolacyjny:";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(97, 137);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xTextBox.TabIndex = 5;
+            // 
+            // fxTextBox
+            // 
+            this.fxTextBox.Location = new System.Drawing.Point(97, 175);
+            this.fxTextBox.Name = "fxTextBox";
+            this.fxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fxTextBox.TabIndex = 6;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(97, 219);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(100, 23);
+            this.calculateBtn.TabIndex = 7;
+            this.calculateBtn.Text = "Oblicz";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // interTaskText
+            // 
+            this.interTaskText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.interTaskText.Location = new System.Drawing.Point(3, 36);
+            this.interTaskText.Name = "interTaskText";
+            this.interTaskText.Size = new System.Drawing.Size(311, 98);
+            this.interTaskText.TabIndex = 8;
+            this.interTaskText.Text = "task";
+            this.interTaskText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(162, 261);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(34, 13);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.Text = "wynik";
+            // 
             // InterpolationMenuControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.interTaskText);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.fxTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.interLabel);
+            this.Controls.Add(this.fxLabel);
+            this.Controls.Add(this.xLabel);
             this.Controls.Add(this.backBtn);
             this.Controls.Add(this.interpolationTitle);
             this.Name = "InterpolationMenuControl";
             this.Size = new System.Drawing.Size(317, 360);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +158,13 @@
 
         private System.Windows.Forms.Label interpolationTitle;
         private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label fxLabel;
+        private System.Windows.Forms.Label interLabel;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox fxTextBox;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label interTaskText;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
--- InterpolationMenuControl.cs
+++ InterpolationMenuControl.cs
@@ -1,21 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MathNet.Numerics.Interpolation;
 
 namespace numericalMethods
 {
     public partial class InterpolationMenuControl : UserControl
     {
         public event EventHandler BackButtonClicked;
+
         public InterpolationMenuControl()
         {
             InitializeComponent();
+            interTaskText.Text = "Napisz i uruchom aplikację komputerową, która na podstawie wprowadzonych czterech par wielkości (x, f(x)) znajduje wielomian interpolacyjny trzeciego stopnia przybliżający tę funkcję f(x).Jeżeli wykonanie tej operacji jest niemożliwe, to program powinien poinformować o tym użytkownika.";
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            string xInput = xTextBox.Text;
+            string fxInput = fxTextBox.Text;
+
+            try
+            {
+                double[] xValues = ParseInput(xInput);
+                double[] fxValues = ParseInput(fxInput);
+
+                if (xValues.Length == fxValues.Length && xValues.Length >= 4)
+                {
+                    // Create cubic spline interpolation
+                    var spline = CubicSpline.InterpolateNatural(xValues, fxValues);
+
+                    // Generate textual representation of the polynomial
+                    string polynomialText = GeneratePolynomialText(spline);
+
+                    resultLabel.Text = polynomialText;
+                }
+                else
+                {
+                    MessageBox.Show("Wprowadź co najmniej 4 pary (x, f(x)) danych.");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Nieprawidłowy format danych. Wprowadź liczby oddzielone przecinkiem.");
+            }
+        }
+
+        private string GeneratePolynomialText(IInterpolation spline)
+        {
+            // Build textual representation of the polynomial
+            string polynomialText = "P(x) = ";
+
+            for (int i = 0; i < spline.PointCount - 1; i++)
+            {
+                if (i > 0)
+                {
+                    polynomialText += spline.Coefficients[i][2] > 0 ? " + " : " - ";
+                }
+                polynomialText += Math.Abs(spline.Coefficients[i][2]);
+
+                if (i < spline.PointCount - 2)
+                {
+                    polynomialText += "(x - " + spline.Coefficients[i][1] + ")";
+                }
+            }
+            return polynomialText;
+        }
+
+
+        private double[] ParseInput(string input)
+        {
+            // Split input data using a comma
+            string[] values = input.Split(',');
+
+            // Convert strings to numbers
+            return values.Select(v => double.Parse(v)).ToArray();
         }
 
         private void backBtn_Click(object sender, EventArgs e)

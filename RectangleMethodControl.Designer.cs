dc47a4dce4190bb3bd776d1e224e5d4ec1011c60
N0va1992 <bekierpatryk311@gmail.com>
2023-12-12 17:01:19 +0100

    rectangle mathod calculates added. Localization added. Everything work properly

--- RectangleMethodControl.Designer.cs
+++ RectangleMethodControl.Designer.cs
@@ -29,6 +29,18 @@
         private void InitializeComponent()
         {
             this.backBtn11 = new System.Windows.Forms.Button();
+            this.rectangleTitle = new System.Windows.Forms.Label();
+            this.coefficientsTextBox = new System.Windows.Forms.TextBox();
+            this.accuracyTextBox = new System.Windows.Forms.TextBox();
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.bTextBox = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.coefficientsLabel = new System.Windows.Forms.Label();
+            this.aLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.accuracyLabel = new System.Windows.Forms.Label();
+            this.resultTxt = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // backBtn11
@@ -42,19 +54,147 @@
             this.backBtn11.UseVisualStyleBackColor = true;
             this.backBtn11.Click += new System.EventHandler(this.backBtn11_Click);
             // 
+            // rectangleTitle
+            // 
+            this.rectangleTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rectangleTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rectangleTitle.Location = new System.Drawing.Point(0, 0);
+            this.rectangleTitle.Name = "rectangleTitle";
+            this.rectangleTitle.Size = new System.Drawing.Size(334, 37);
+            this.rectangleTitle.TabIndex = 13;
+            this.rectangleTitle.Text = "tytuł";
+            this.rectangleTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // coefficientsTextBox
+            // 
+            this.coefficientsTextBox.Location = new System.Drawing.Point(36, 94);
+            this.coefficientsTextBox.Name = "coefficientsTextBox";
+            this.coefficientsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.coefficientsTextBox.TabIndex = 14;
+            // 
+            // accuracyTextBox
+            // 
+            this.accuracyTextBox.Location = new System.Drawing.Point(166, 94);
+            this.accuracyTextBox.Name = "accuracyTextBox";
+            this.accuracyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.accuracyTextBox.TabIndex = 15;
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Location = new System.Drawing.Point(36, 148);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(100, 20);
+            this.aTextBox.TabIndex = 16;
+            // 
+            // bTextBox
+            // 
+            this.bTextBox.Location = new System.Drawing.Point(166, 148);
+            this.bTextBox.Name = "bTextBox";
+            this.bTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bTextBox.TabIndex = 17;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(101, 194);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(100, 23);
+            this.calculateBtn.TabIndex = 18;
+            this.calculateBtn.Text = "Oblicz";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(101, 257);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(35, 13);
+            this.resultLabel.TabIndex = 19;
+            this.resultLabel.Text = "label1";
+            // 
+            // coefficientsLabel
+            // 
+            this.coefficientsLabel.AutoSize = true;
+            this.coefficientsLabel.Location = new System.Drawing.Point(33, 78);
+            this.coefficientsLabel.Name = "coefficientsLabel";
+            this.coefficientsLabel.Size = new System.Drawing.Size(80, 13);
+            this.coefficientsLabel.TabIndex = 20;
+            this.coefficientsLabel.Text = "Współczynniki:";
+            // 
+            // aLabel
+            // 
+            this.aLabel.AutoSize = true;
+            this.aLabel.Location = new System.Drawing.Point(39, 132);
+            this.aLabel.Name = "aLabel";
+            this.aLabel.Size = new System.Drawing.Size(17, 13);
+            this.aLabel.TabIndex = 21;
+            this.aLabel.Text = "A:";
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(164, 132);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(17, 13);
+            this.bLabel.TabIndex = 22;
+            this.bLabel.Text = "B:";
+            // 
+            // accuracyLabel
+            // 
+            this.accuracyLabel.AutoSize = true;
+            this.accuracyLabel.Location = new System.Drawing.Point(164, 78);
+            this.accuracyLabel.Name = "accuracyLabel";
+            this.accuracyLabel.Size = new System.Drawing.Size(69, 13);
+            this.accuracyLabel.TabIndex = 23;
+            this.accuracyLabel.Text = "Dokładność:";
+            // 
+            // resultTxt
+            // 
+            this.resultTxt.AutoSize = true;
+            this.resultTxt.Location = new System.Drawing.Point(3, 257);
+            this.resultTxt.Name = "resultTxt";
+            this.resultTxt.Size = new System.Drawing.Size(77, 13);
+            this.resultTxt.TabIndex = 24;
+            this.resultTxt.Text = "Wartość całki:";
+            // 
             // RectangleMethodControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.resultTxt);
+            this.Controls.Add(this.accuracyLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.aLabel);
+            this.Controls.Add(this.coefficientsLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.bTextBox);
+            this.Controls.Add(this.aTextBox);
+            this.Controls.Add(this.accuracyTextBox);
+            this.Controls.Add(this.coefficientsTextBox);
+            this.Controls.Add(this.rectangleTitle);
             this.Controls.Add(this.backBtn11);
             this.Name = "RectangleMethodControl";
             this.Size = new System.Drawing.Size(334, 425);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button backBtn11;
+        private System.Windows.Forms.Label rectangleTitle;
+        private System.Windows.Forms.TextBox coefficientsTextBox;
+        private System.Windows.Forms.TextBox accuracyTextBox;
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.TextBox bTextBox;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label coefficientsLabel;
+        private System.Windows.Forms.Label aLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label accuracyLabel;
+        private System.Windows.Forms.Label resultTxt;
     }
 }
--- RectangleMethodControl.cs
+++ RectangleMethodControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using numericalMethods.Localization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,11 +18,74 @@ namespace numericalMethods
         public RectangleMethodControl()
         {
             InitializeComponent();
+            resultLabel.Text = string.Empty;
+            UpdateLocalizedString();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            //pobieranie współczynników wielomianu
+            string[] coefficientsStr = coefficientsTextBox.Text.Split(',');
+            double[] coefficients = new double[coefficientsStr.Length];
+            for (int i = 0; i < coefficientsStr.Length; i++)
+            {
+                coefficients[i] = double.Parse(coefficientsStr[i]);
+            }
+
+            //pobieranie przedziału [a, b] i dokładności
+            double a = double.Parse(aTextBox.Text);
+            double b = double.Parse(bTextBox.Text);
+            double accuracy = double.Parse(accuracyTextBox.Text);
+
+            //obliczanie wartości całko metodą prostokątów
+            double result = RectangleMethod(a, b, accuracy, coefficients);
+
+            //wyświetlanie wyniku
+            resultLabel.Text = result.ToString();
         }
 
         private void backBtn11_Click(object sender, EventArgs e)
         {
             BackButtonClicked?.Invoke(this, EventArgs.Empty);
         }
+
+        private double RectangleMethod(double a, double b, double accuracy, double[] coefficients)
+        {
+            int rectangleQuantity = 1000; //liczba prostokątów
+            double h = (b - a) / rectangleQuantity;
+            double sum = 0;
+
+            for (int i = 0; i < rectangleQuantity; i++)
+            {
+                double x = a + i * h;
+                double rectangleArea = h * PolynomialValue(x, coefficients);
+                sum += rectangleArea;
+            }
+            return sum;
+        }
+
+        private double PolynomialValue(double x, double[] coefficients)
+        {
+            double result = 0;
+
+            for (int i = 0;i < coefficients.Length;i++)
+            {
+                result += coefficients[i] * Math.Pow(x, i);
+            }
+
+            return result;
+        }
+
+        private void UpdateLocalizedString()
+        {
+            calculateBtn.Text = LanguageManager.GetLocalizedString("calculateBtn");
+            backBtn11.Text = LanguageManager.GetLocalizedString("backBtn");
+            rectangleTitle.Text = LanguageManager.GetLocalizedString("rectangleTitle");
+            coefficientsLabel.Text = LanguageManager.GetLocalizedString("coefficientsLabel");
+            accuracyLabel.Text = LanguageManager.GetLocalizedString("accuracyLabel");
+            resultTxt.Text = LanguageManager.GetLocalizedString("resultTxt");
+        }
+
+        
     }
 }

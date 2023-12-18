bfaa5ee44dc3ef3c339024a9bafc30eada42b8ab
N0va1992 <bekierpatryk311@gmail.com>
2023-12-18 19:06:00 +0100

    proggresivve different calculation added. dont work. System.OutOfMemoryException

--- ProgressiveMenuControl.Designer.cs
+++ ProgressiveMenuControl.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.backBtn11 = new System.Windows.Forms.Button();
             this.progressiveTitle = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.fxTextBox = new System.Windows.Forms.TextBox();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.fxLabel = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // backBtn11
@@ -54,15 +62,84 @@
             this.progressiveTitle.Text = "Różnice progresywne";
             this.progressiveTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(37, 80);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xTextBox.TabIndex = 20;
+            // 
+            // fxTextBox
+            // 
+            this.fxTextBox.Location = new System.Drawing.Point(178, 80);
+            this.fxTextBox.Name = "fxTextBox";
+            this.fxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fxTextBox.TabIndex = 21;
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(14, 83);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(17, 13);
+            this.xLabel.TabIndex = 22;
+            this.xLabel.Text = "X:";
+            // 
+            // fxLabel
+            // 
+            this.fxLabel.AutoSize = true;
+            this.fxLabel.Location = new System.Drawing.Point(145, 83);
+            this.fxLabel.Name = "fxLabel";
+            this.fxLabel.Size = new System.Drawing.Size(27, 13);
+            this.fxLabel.TabIndex = 23;
+            this.fxLabel.Text = "F(x):";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(38, 115);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(100, 23);
+            this.addBtn.TabIndex = 24;
+            this.addBtn.Text = "Dodaj";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(178, 115);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(100, 23);
+            this.calculateBtn.TabIndex = 25;
+            this.calculateBtn.Text = "Oblicz";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 119);
+            this.dataGridView1.TabIndex = 26;
+            // 
             // ProgressiveMenuControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.fxLabel);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.fxTextBox);
+            this.Controls.Add(this.xTextBox);
             this.Controls.Add(this.progressiveTitle);
             this.Controls.Add(this.backBtn11);
             this.Name = "ProgressiveMenuControl";
             this.Size = new System.Drawing.Size(336, 421);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +147,12 @@
 
         private System.Windows.Forms.Button backBtn11;
         private System.Windows.Forms.Label progressiveTitle;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox fxTextBox;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label fxLabel;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- ProgressiveMenuControl.cs
+++ ProgressiveMenuControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using numericalMethods.Localization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,14 +14,107 @@ namespace numericalMethods
     public partial class ProgressiveMenuControl : UserControl
     {
         public event EventHandler BackButtonClicked;
+
+        private List<double> xValues;
+        private List<double> yValues;
+
         public ProgressiveMenuControl()
         {
             InitializeComponent();
+
+            xValues = new List<double>();
+            yValues = new List<double>();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            //dodawanie punktów do list
+            double x, y;
+            if (Double.TryParse(xTextBox.Text, out x) && Double.TryParse(fxTextBox.Text, out y))
+            {
+                xValues.Add(x);
+                yValues.Add(y);
+                MessageBox.Show(LanguageManager.GetLocalizedString("addMsg") + "(" + x + ", " + y + ")");
+            }
+            else
+            {
+                MessageBox.Show(LanguageManager.GetLocalizedString("errorMsg"));
+            }
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            // sprawdzenie czy istnieje wystarczająca ilość punktów
+            if (xValues.Count < 2)
+            {
+                MessageBox.Show(LanguageManager.GetLocalizedString("notEnoughtMsg"));
+                return;
+            }
+
+            //obliczenia różnic progresywnych
+            List<List<double>> progressiveDiff = CalculateProgDiff();
+
+            //wyświetlanie wyników w DataGridView
+            UpdateDataGridView(progressiveDiff);
+        }
+
+        private List<List<double>> CalculateProgDiff()
+        {
+            List<List<double>> progressiveDiff = new List<List<double>>();
+            progressiveDiff.Add(new List<double>(yValues));
+
+            for (int j = 1; j <yValues.Count; j++)
+            {
+                List<double> diffLevel = new List<double>();
+
+                for (int i = 0; i < yValues.Count - j; i++)
+                {
+                    double diff = progressiveDiff[j - 1][i + 1] - progressiveDiff[j - 1][i];
+                    diffLevel.Add(diff);
+                }
+                progressiveDiff.Add(diffLevel);
+            }
+            return progressiveDiff;
+        }
+
+        private void UpdateDataGridView(List<List<double>> progressiveDiff)
+        {
+            // wyczyszczenie tabeli
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            //dodanie kolumn do DGV
+            for (int i = 0; i < progressiveDiff.Count; i++)
+            {
+                dataGridView1.Columns.Add(LanguageManager.GetLocalizedString("column") + i, LanguageManager.GetLocalizedString("different") + i);
+            }
+
+            //dodanie danych do DGV
+            for (int i = 0; i < progressiveDiff[0].Count; i++)
+            {
+                List<string> row = new List<string>();
+
+                for (int j = 0; j < progressiveDiff.Count;)
+                {
+                    //sprawdzenie czy lista ma wystarczająca pojemność
+                    if (i < progressiveDiff[j].Count)
+                    {
+                        row.Add(progressiveDiff[j][i].ToString());
+                    }
+                    else
+                    {
+                        row.Add("");
+                    }
+                    
+                }
+                dataGridView1.Rows.Add(row.ToArray());
+            }
         }
 
         private void backBtn11_Click(object sender, EventArgs e)
         {
             BackButtonClicked?.Invoke(this, EventArgs.Empty);
         }
+       
     }
 }
